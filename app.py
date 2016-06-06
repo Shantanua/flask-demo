eb879084ae0c7102ad0a99803cc44387a48056b6
Shantanua <spawar@ncsu.edu>
2016-06-05 22:02:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -9,6 +11,43 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['ticker']
+    splot(text)
+    return render_template('timeseries.html')
+  
+def splot(text):
+    url1 = 'https://www.quandl.com/api/v3/datasets/WIKI/'
+    url2 = text
+    url3 = '/data.json?start_date=2015-05-01&end_date=2015-05-31'
+    
+    url = url1 + url2 + url3
+    
+    r = requests.get(url)
+    
+    data = r.json()
+    data2 = data['dataset_data']['data']
+    data3 = pd.DataFrame(data2,columns = data['dataset_data']['column_names'])
+
+    from bokeh.charts import TimeSeries, show, output_file, vplot
+
+    data = dict(
+        AAPL=data3['Adj. Close'],
+        Date=data3['Date']
+    )
+
+    tsline = TimeSeries(data,
+        x='Date', y=['AAPL'],
+        color=['AAPL'], dash=['AAPL'],
+        title="Timeseries", ylabel='Stock Prices', legend=True)
+
+    output_file("timeseries.html")
+
+    show(vplot(tsline))
+    
 
 if __name__ == '__main__':
   app.run(port=33507)

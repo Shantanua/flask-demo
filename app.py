ffc917819f78057f9ff7dab39e9c3b8f27f44c5f
Shantanua <spawar@ncsu.edu>
2016-06-05 22:38:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect
-
+import pandas as pd
+import requests
+from bokeh.charts import TimeSeries, show, output_file, vplot
+from bokeh.embed import components 
 
 app = Flask(__name__)
 
@@ -11,8 +14,32 @@ def main():
 def my_form_post():
 
     text = request.form['ticker']
-    #splot(text)
-    return render_template('index.html')
+    url1 = 'https://www.quandl.com/api/v3/datasets/WIKI/'
+    url2 = text
+    url3 = '/data.json?start_date=2015-05-01&end_date=2015-05-31'
+    
+    url = url1 + url2 + url3
+    
+    r = requests.get(url)
+    
+    data = r.json()
+    data2 = data['dataset_data']['data']
+    data3 = pd.DataFrame(data2,columns = data['dataset_data']['column_names'])
+
+
+    data = dict(
+        AAPL=data3['Adj. Close'],
+        Date=data3['Date']
+    )
+
+    tsline = TimeSeries(data,
+        x='Date', y=['AAPL'],
+        color=['AAPL'], dash=['AAPL'],
+        title="Timeseries", ylabel='Stock Prices', legend=True)
+
+
+    script, div = components(tsline)
+    return render_template('graph.html', script=script, div=div)
    
 
 if __name__ == '__main__':

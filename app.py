8ad773a8b93e2be533e8b73a28fe73e775860f6c
Shantanua <spawar@ncsu.edu>
2016-06-05 22:46:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ def my_form_post():
     url3 = '/data.json?start_date=2015-05-01&end_date=2015-05-31'
     
     url = url1 + url2 + url3
-    
+    print url
     r = requests.get(url)
     
     data = r.json()
+    print data
     data2 = data['dataset_data']['data']
     data3 = pd.DataFrame(data2,columns = data['dataset_data']['column_names'])
 
